9d843390d7cc12e46f71f5ebcd5afa1bb05932b5
Mark Sheehan <mark.sheehan@sharp-ls.eu>
2020-04-06 16:26:18 +0100

    Cake server python

---
+++ Python/server.py
@@ -0,0 +1,41 @@
+from concurrent import futures
+import logging
+
+import grpc
+
+import cake_pb2
+import cake_pb2_grpc
+
+
+class Greeter(cake_pb2_grpc.GreeterServicer):
+
+    people_who_get_extra_cake = {"john": 2,
+                                 "nelson": 2,
+                                 "helen": 2,
+                                 "aengus": 2}
+
+    def SayHello(self, request, context):
+        caller = request.name
+
+        pieces_of_cake = 0
+        if caller in self.people_who_get_extra_cake:
+            pieces_of_cake += self.people_who_get_extra_cake[caller]
+
+        message = f"Wow you get {pieces_of_cake} pieces of cake"
+
+        response = cake_pb2.AmountOfCakeMessage(numberOfCakes=pieces_of_cake, message=message)
+
+        return response
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    cake_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
